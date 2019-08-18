0bec844cef0cf39e52c27ffc568f4a2f1c4142f6
yeay <yueraiyu@yeah.com>
2019-08-18 09:00:18 +0800

    配置端口

--- frontend/src/settings.js
+++ frontend/src/settings.js
@@ -2,6 +2,6 @@ import axios from 'axios'
 
 // 基础配置
 axios.defaults.timeout = 5000  // 超时时间
-axios.defaults.baseURL = 'http://localhost:5000'
+axios.defaults.baseURL = 'http://127.0.0.1:8098'
 
 export default axios
