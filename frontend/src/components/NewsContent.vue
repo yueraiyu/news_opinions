96ec025eab6423a73ab8ba4325ed1e85451fca59
yeay <yueraiyu@yeah.com>
2019-08-25 15:39:58 +0800

    导航栏调整

--- frontend/src/components/NewsContent.vue
+++ frontend/src/components/NewsContent.vue
@@ -1,27 +1,5 @@
 <template>
   <div>
-    <!-- Navigation -->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-      <div class="container">
-        <a class="navbar-brand" href="#"> 问就是全栈 - 新闻分析 </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item active">
-              <router-link v-bind:to="{ name: 'News'}" class="nav-link"> 论点
-                <span class="sr-only">(current)</span>
-              </router-link>
-            </li>
-            <li class="nav-item">
-              <router-link v-bind:to="{ name: 'NewsAnalyzeTxt'}" class="nav-link">分析</router-link>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-
     <!-- Page Content -->
     <div class="container">
       <div class="row">
